0f4144daa0d216e929099856635807a17df02386
Kyoya <jackma8569@gmail.com>
2023-12-13 22:53:08 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
+import nftContractABI from './component/abi/nftContract.json';
 import './App.css';
 
+// ここにNFTスマートコントラクトのアドレスとABIを設定します
+const nftContractAddress = '0x62137c044e65D50E8f3d6512A69488FF5C62a7A7';
+
 function App() {
+  const [userAddress, setUserAddress] = useState('');
+  const [nftOwned, setNftOwned] = useState(false);
+
+  const connectWalletHandler = async () => {
+    if (window.ethereum) {
+      try {
+        const newAccounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        setUserAddress(newAccounts[0]);
+        checkNFTOwnership(newAccounts[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.log('Install MetaMask');
+    }
+  };
+
+  const checkNFTOwnership = async (userAddress) => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(nftContractAddress, nftContractABI, provider);
+      
+      // ここでNFTの所有権を確認します。NFTスマートコントラクトによっては、メソッド名が異なる場合があります。
+      const balance = await contract.balanceOf(userAddress,2);
+      if (balance > 0) setNftOwned(true);
+      setNftOwned(balance.gt(0));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="title">Discord Server</h1>
+      <button className="connect-button" onClick={connectWalletHandler}>Connect to MetaMask</button>
+      {userAddress && <p className="address">Connected: {userAddress}</p>}
+      <p className="nft-status">{nftOwned ? 'You own the NFT!' : 'You do not own the NFT'}</p>
+    </>
   );
 }
 
